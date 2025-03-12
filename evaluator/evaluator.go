3553ba87019644674d545defcd99466a79bda6ce
TheGamingGod85 <The.Gaming.God85@gmail.com>
2025-03-12 20:14:20 +0530

    add support for built-in functions, including 'len', and refactor evaluator for improved functionality

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -140,11 +140,15 @@ func evalBlockStatement(block *ast.BlockStatement, env *object.Environment) obje
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("%s", "identifier not found: "+node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
-	return val
+
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+
+	return newError("%s", "identifier not found: "+node.Value)
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
@@ -184,14 +188,16 @@ func unwrapReturnValue(obj object.Object) object.Object {
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	case *object.Builtin:
+		return fn.Fn(args...)
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 func evalBangOperatorExpression(right object.Object) object.Object {
