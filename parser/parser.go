2dce9ba95e97d6afebf569933a77e6d20ce0e05a
TheGamingGod85 <The.Gaming.God85@gmail.com>
2025-04-02 08:02:12 +0530

    add tracer messages back

--- parser/parser.go
+++ parser/parser.go
@@ -222,6 +222,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 // parseExpressionStatement parses an expression statement and returns an ast.ExpressionStatement.
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	defer untrace(trace("parseExpressionStatement"))
 	stmt := &ast.ExpressionStatement{Token: p.curToken}
 
 	stmt.Expression = p.parseExpression(LOWEST)
@@ -235,6 +236,7 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 
 // parseExpression parses an expression and returns an ast.Expression.
 func (p *Parser) parseExpression(precedence int) ast.Expression {
+	defer untrace(trace("parseExpression"))
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
@@ -281,6 +283,7 @@ func (p *Parser) parseIdentifier() ast.Expression {
 
 // parseIntegerLiteral parses an integer literal and returns an ast.IntegerLiteral.
 func (p *Parser) parseIntegerLiteral() ast.Expression {
+	defer untrace(trace("parseIntegerLiteral"))
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 
 	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
@@ -312,6 +315,7 @@ func (p *Parser) parseFloatLiteral() ast.Expression {
 
 // parsePrefixExpression parses a prefix expression and returns an ast.PrefixExpression.
 func (p *Parser) parsePrefixExpression() ast.Expression {
+	defer untrace(trace("parsePrefixExpression"))
 	expression := &ast.PrefixExpression{
 		Token:    p.curToken,
 		Operator: p.curToken.Literal,
@@ -326,6 +330,7 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 
 // parseInfixExpression parses an infix expression and returns an ast.InfixExpression.
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+	defer untrace(trace("parseInfixExpression"))
 	expression := &ast.InfixExpression{
 		Token:    p.curToken,
 		Operator: p.curToken.Literal,
